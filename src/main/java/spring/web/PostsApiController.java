8e92e1151190161f42ebe58cc09e934d803161f0
sdardew <sdardew@gmail.com>
2021-09-27 00:01:04 +0900

    Add PostApiController.java

--- src/main/java/spring/web/PostsApiController.java
+++ src/main/java/spring/web/PostsApiController.java
@@ -1,2 +1,19 @@
-package spring.web;public class PostsApiController {
+package spring.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import spring.service.posts.PostsService;
+import spring.web.dto.PostsSaveRequestDto;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
 }

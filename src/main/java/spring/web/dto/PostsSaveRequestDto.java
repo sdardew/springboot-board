caa1db33e9bee8bcef028650ef114774c3e3c675
sdardew <sdardew@gmail.com>
2021-09-27 00:00:25 +0900

    Add PostSaveRequestDto.java

--- src/main/java/spring/web/dto/PostsSaveRequestDto.java
+++ src/main/java/spring/web/dto/PostsSaveRequestDto.java
@@ -1,2 +1,29 @@
-package spring.web.dto;public class PostsSaveRequestDto {
+package spring.web.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import spring.domain.posts.Posts;
+
+@Getter
+@NoArgsConstructor
+public class PostsSaveRequestDto {
+    private String title;
+    private String content;
+    private String author;
+
+    @Builder
+    public PostsSaveRequestDto(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public Posts toEntity() {
+        return Posts.builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .build();
+    }
 }

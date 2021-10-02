495fd72d6adcf8c5bb11ea10a107285c12a95a0d
sdardew <sdardew@gmail.com>
2021-10-02 23:41:35 +0900

    Add Google Login

--- src/main/java/spring/config/auth/SecurityConfig.java
+++ src/main/java/spring/config/auth/SecurityConfig.java
@@ -7,26 +7,25 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import spring.domain.user.Role;
 
 @RequiredArgsConstructor
-@EnableWebSecurity
+@EnableWebSecurity // Spring Security 설정들을 활정화
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final CustomOAuth2UserService customOAuth2UserService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
-                .headers().frameOptions().disable()
+                .csrf().disable().headers().frameOptions().disable() // h2-console 화면을 사용하기 위한 설정
                 .and()
-                    .authorizeRequests()
-                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
-                    .antMatchers("/api/v1/**").hasRole(Role.USER.name())
-                    .anyRequest().authenticated()
+                    .authorizeRequests() // URL별 권한 관리를 설정하는 옵션의 시작점
+                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll() // 전체 열람 권한
+                    .antMatchers("/api/v1/**").hasRole(Role.USER.name()) // USER 권한을 가진 사람만
+                    .anyRequest().authenticated() // anyReQuest(): 설정된 값들 이외 나머지 URL, authenticated
                 .and()
-                    .logout()
-                        .logoutSuccessUrl("/")
+                    .logout() // 로그아웃 시
+                        .logoutSuccessUrl("/") /// 로 이동
                 .and()
-                    .oauth2Login()
-                        .userInfoEndpoint()
-                            .userService(customOAuth2UserService);
+                    .oauth2Login() // oauth2 로그인
+                        .userInfoEndpoint() // 로그인 이후 사용자 정보를 가져올 때 설정
+                            .userService(customOAuth2UserService); // 로그인 성공 시 진행할 인터페이스 구현체 등록
     }
 }
--- src/main/java/spring/web/IndexController.java
+++ src/main/java/spring/web/IndexController.java
@@ -1,22 +1,31 @@
 package spring.web;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import spring.config.auth.dto.SessionUser;
 import spring.service.posts.PostsService;
 import spring.web.dto.PostsResponseDto;
+
+import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 

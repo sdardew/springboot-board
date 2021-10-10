8c675a350f68ab7d289f1dd59e4874a7099d0877
sdardew <sdardew@gmail.com>
2021-10-10 23:24:21 +0900

    HelloControllerTest 문제 해결

--- src/test/java/spring/web/HelloControllerTest.java
+++ src/test/java/spring/web/HelloControllerTest.java
@@ -4,6 +4,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -11,16 +14,23 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 import org.springframework.test.web.servlet.ResultActions;
+import spring.config.auth.SecurityConfig;
+
 import static org.hamcrest.Matchers.is;
 
 
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class,
+    excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class) // 스캔 대상이 아닌 것 제거
+    }
+)
 public class HelloControllerTest {
     @Autowired
     private MockMvc mvc;
 
+    @WithMockUser(roles = "USER") // 가짜 사용자 추가
     @Test
     public void hello가_리턴된다() throws Exception {
         String hello = "hello";
@@ -30,6 +40,7 @@ public class HelloControllerTest {
                 .andExpect(content().string(hello));
     }
 
+    @WithMockUser(roles = "USER") // 가짜 사용자 추가
     @Test
     public void helloDto가_리턴된다() throws Exception {
         String name = "hello";
